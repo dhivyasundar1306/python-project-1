706e059de298e2efde2a2739e0c20fcecd1b5fea
dhivyasundar1306 <107749217+dhivyasundar1306@users.noreply.github.com>
2022-06-23 18:42:36 +0530

    Add files via upload

---
+++ filename extension.py.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 23 18:14:44 2022
+
+@author: Jai Sri Krishna
+"""
+
+a=input("Enter filename along with the extension: ")
+ext=a.split(".")
+print("File extension: " ,(ext[-1]))
