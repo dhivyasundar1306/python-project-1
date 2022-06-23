314628669978eb4cf24fa9c0b3c1334258bebe70
dhivyasundar1306 <107749217+dhivyasundar1306@users.noreply.github.com>
2022-06-23 18:45:19 +0530

    Add files via upload

---
+++ radius of the circle.py.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from math import pi
+r=float(input("Enter the radius of the circle: "))
+b=pi*r**2
+print("Area of the circle:",b)
